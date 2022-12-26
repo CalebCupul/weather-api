2e8d1d34ad4116f4d04a28c6c89faecc2b666a80
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2022-12-26 01:06:50 -0600

    added alerts

--- public/app.js
+++ public/app.js
@@ -14,36 +14,49 @@ function successGeolocationCallback(position) {
 }
 
 function errorGeolocationCallback(error) {
+    ui.cleanErrors()
+    ui.alert('We couldn\'t get your location, please refresh your browser and try again.')
 
 }
 
-function getWeatherByCoordinates({coords: { latitude: lat, longitude: lon }}) {
+function getWeatherByCoordinates({ coords: { latitude: lat, longitude: lon } }) {
 
     const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            if ( data.cod === "404" ) {
-                // mostrarError('La ciudad no existe')
+            if (data.cod === "404") {
+                ui.cleanErrors()
+                ui.alert('City not found, please try again.')
+                return
+            } else if (data.cod === '200') {
+                const { city: { name }, list } = data
+                ui.loadCityWeather(name, list)
             }
-            const { city: { name }, list } = data
-            ui.loadCityWeather(name, list)
-        })  
+            return
+
+        })
 }
 
-function getWeatherByCity(e){
+function getWeatherByCity(e) {
     e.preventDefault()
-    
+
     const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput.value}&appid=${key}`
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            if ( data.cod === "404" ) {
-                // mostrarError('La ciudad no existe')
+            if (data.cod === "404") {
+                ui.cleanErrors()
+                ui.alert('City not found, please try again.')
+                return
+            } else if (data.cod === "200") {
+                const { city: { name }, list } = data
+                ui.loadCityWeather(name, list)
             }
-            const { city: { name }, list } = data
-            ui.loadCityWeather(name, list)
-        })  
+
+            return
+
+        })
 }
 
